c5984955e0567c1f10b1759df2efdebd0a2e632c
zinbiel <elias.arnold@tum.de>
2015-12-12 16:08:37 +0100

    update readme

--- runCurl.sh
+++ runCurl.sh
@@ -1,5 +1,5 @@
 curl -H "Accept: application/json" http://localhost:8080/dyn/message/empty  
-myid=$(curl -X POST -H "Content-Type:application/json" http://localhost:8080/dyn/message/stage -d '{"appTag":null,"service":"myservice","stringMap":{"testKey":"testValue"},"integerMap":{},"floatMap":{},"binaryMap":{},"metaData":{}}')
-curl -H "Accept: application/json" http://localhost:8080/dyn/message/publish/$myid
-curl -H "Accept: application/json" http://localhost:8080/dyn/service/myservice
-curl -H "Accept: application/json" http://localhost:8080/dyn/subscribe/myservice
+myid=$(curl -X POST -H "Content-Type:application/json" http://localhost/dyn/message/stage -d '{"appTag":null,"service":"myservice","stringMap":{"testKey":"testValue"},"integerMap":{},"floatMap":{},"binaryMap":{},"metaData":{}}')
+curl -H "Accept: application/json" http://localhost/dyn/message/publish/$myid
+curl -H "Accept: application/json" http://localhost/dyn/service/myservice
+curl -H "Accept: application/json" http://localhost/dyn/subscribe/myservice
