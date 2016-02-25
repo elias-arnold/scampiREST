82fffdeea7aface6db2a6ccc71c74165e4bc066e
Elias Arnold <elias.arnold@tum.de>
2016-02-25 17:21:46 +0100

    Update test case
    test case update using different pictures without copyright problems.

--- src/test/html-Test-Values/Test-3/test-3.sh
+++ src/test/html-Test-Values/Test-3/test-3.sh
@@ -1,27 +1,31 @@
 serviceName=$1
 echo -e "\nstage empty message"
-muhh='{"appTag":null,"service":"'$serviceName'","stringMap":{"name":"asdasd-asdasd"},"integerMap":{"testIntKey":1},"floatMap":{},"binaryMap":{},"metaData":{}}'
+muhh='{"appTag":null,"service":"'$serviceName'","stringMap":{"name":"testname"},"integerMap":{"testIntKey":1},"floatMap":{},"binaryMap":{},"metaData":{}}'
 
 echo $muhh
 
-myid=$(curl -X POST -H "Content-Type:application/json" http://localhost/dyn/message/stage -d $muhh)
+myid=$(curl -X POST -H "Content-Type:application/json" http://myliberouter.org/dyn/message/stage -d $muhh)
 
 echo -e "\n\n\n$myid\n\n\n"
 read myid
 echo -e "\nupload a index zip"
-curl -v -F name="scampiRest-picture-demo" -F key="main" -F id="$myid" -F file=@"./scampiRest-picture-demo.zip" http://localhost/dyn/upload
+curl -v -F name="scampiRest-picture-demo" -F key="main" -F id="$myid" -F file=@"./scampiRest-picture-demo.zip" http://myliberouter.org/dyn/upload
 echo -e "\nupload sucessfull"
 
 echo -e "\nupload a index zip"
-curl -v -F name="yoga-girl.jpeg" -F key="picture" -F id="$myid" -F file=@"./yoga-girl.jpeg.zip" http://localhost/dyn/upload
+curl -v -F name="Hamlet-1.jpeg" -F key="picture" -F id="$myid" -F file=@"./Hamlet-1.jpeg.zip" http://myliberouter.org/dyn/upload
+echo -e "\nupload sucessfull"
+
+echo -e "\nupload a index zip"
+curl -v -F name="Emilio-1.jpeg" -F key="picture2" -F id="$myid" -F file=@"./Emilio-1.jpeg.zip" http://myliberouter.org/dyn/upload
 echo -e "\nupload sucessfull"
 
 echo -e "\npublish the message"
-curl -H "Accept: application/json" http://localhost/dyn/message/publish/$myid
+curl -H "Accept: application/json" http://myliberouter.org/dyn/message/publish/$myid
 echo -e "\npublish sucessful"
 
-curl -H "Accept: application/json" http://localhost/dyn/subscribe/$serviceName
+curl -H "Accept: application/json" http://myliberouter.org/dyn/subscribe/$serviceName
 
 echo -e "\nget service"
-curl -H "Accept: application/json" http://localhost/dyn/service/$serviceName
+curl -H "Accept: application/json" http://myliberouter.org/dyn/service/$serviceName
 
