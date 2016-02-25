73449ffb668136a9a7bb9ec1a590c98c2b359c5b
Elias Arnold <elias.arnold@tum.de>
2016-02-25 19:47:01 +0100

    update testskript

--- src/test/html-Test-Values/Test-3/test-3.sh
+++ src/test/html-Test-Values/Test-3/test-3.sh
@@ -13,11 +13,11 @@ curl -v -F name="scampiRest-picture-demo" -F key="main" -F id="$myid" -F file=@"
 echo -e "\nupload sucessfull"
 
 echo -e "\nupload a index zip"
-curl -v -F name="Hamlet-1.jpeg" -F key="picture" -F id="$myid" -F file=@"./Hamlet-1.jpeg.zip" http://myliberouter.org/dyn/upload
+curl -v -F name="Hamlet-1.jpeg" -F key="picture" -F id="$myid" -F file=@"./Hamlet-1.jpg.zip" http://myliberouter.org/dyn/upload
 echo -e "\nupload sucessfull"
 
 echo -e "\nupload a index zip"
-curl -v -F name="Emilio-1.jpeg" -F key="picture2" -F id="$myid" -F file=@"./Emilio-1.jpeg.zip" http://myliberouter.org/dyn/upload
+curl -v -F name="Emilio-1.jpeg" -F key="picture2" -F id="$myid" -F file=@"./Emilio-1.jpg.zip" http://myliberouter.org/dyn/upload
 echo -e "\nupload sucessfull"
 
 echo -e "\npublish the message"
